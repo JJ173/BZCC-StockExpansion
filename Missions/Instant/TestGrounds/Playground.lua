af14d3bbe6679c57da25f2dec981cdd1ea9d6119
JJ173 <94978675+JJ173@users.noreply.github.com>
2026-03-14 19:15:09 +0000

    Added New Regime Variant

--- Missions/Instant/TestGrounds/Playground.lua
+++ Missions/Instant/TestGrounds/Playground.lua
@@ -1,14 +1,7 @@
 function Start()
-    BuildObject("ibrecy", 1, "Recycler")
-
-    local scout = BuildObject("ivscout", 1, GetPositionNear("Recycler", 20, 40))
-    SetAsUser(scout, 1)
-
-    local apc = BuildObject("ivapc", 1, GetPositionNear("Recycler", 20, 40))
-    local power = BuildObject("ibpgen", 5, "RecyclerEnemy")
-    Attack(apc, power, 1)
+    local recy = BuildObject("ivrecy_nr", 1, GetPositionNear(GetPosition("Recycler"), 20, 50))
+    SetBestGroup(recy)
 end
 
 function Update()
-
 end
